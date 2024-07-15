4d6a4b21687e7c1e3fc3048f7a3afa5ce4162548
raaulrodriguees <clovisadavildo@gmail.com>
2024-07-15 09:43:40 -0300

    Criado os arquivos e adicionando palavras na list

---
+++ Lógica/palavra.py
@@ -0,0 +1,5 @@
+import random
+
+class Palavra:
+    def __init__(self, lita_palavra):
+        self.palavra = random.choice(lita_palavra)
---
+++ Principal/lista_palavras.py
@@ -0,0 +1,16 @@
+temas = {
+    'animais': ['Cachorro', 'Gato', 'Elefante', 'Girafa', 'Leão', 'Tigre', 'Urso', 'Cavalo', 'Vaca', 'Ovelha', 'Cabra',
+                'Porco', 'Galinha', 'Pato', 'Ganso', 'Peixe', 'Coelho', 'Camundongo', 'Papagaio', 'Tartaruga',
+                'Canguru', 'Rinoceronte', 'Hipopótamo', 'Crocodilo', 'Jacaré', 'Pinguim', 'Águia', 'Coruja', 'Falcão',
+                'Tucano', 'Arara', 'Tubarão', 'Golfinho', 'Baleia', 'Polvo', 'Caranguejo', 'Lagosta', 'Esquilo', 'Cobra', 'Sapo'],
+
+    'frutas': ['Maçã', 'Banana', 'Laranja', 'Uva', 'Abacaxi', 'Manga', 'Melancia', 'Melão', 'Morango', 'Limão', 'Pêssego',
+               'Pera', 'Ameixa', 'Caqui', 'Cereja', 'Coco', 'Goiaba', 'Kiwi', 'Mamão', 'Maracujá', 'Abacate',
+               'Jabuticaba', 'Acerola', 'Pitanga', 'Carambola', 'Framboesa', 'Amora', 'Graviola', 'Jaca',
+               'Lichia', 'Nectarina', 'Romã', 'Tangerina', 'Caju', 'Figo', 'Cambuci', 'Guaraná', 'Cupuaçu', 'Jenipapo', 'Açaí'],
+
+    'paises': ['Brasil', 'Estados Unidos', 'Canadá', 'México', 'Argentina', 'Colômbia', 'Peru', 'Chile', 'Uruguai', 'Paraguai', 'Bolívia',
+               'Venezuela', 'Equador', 'Costa Rica', 'Panamá', 'Guatemala', 'Honduras', 'El Salvador', 'Nicarágua', 'Cuba',
+               'Haiti', 'República Dominicana', 'Jamaica', 'Bahamas', 'Bangladesh', 'Alemanha', 'França', 'Itália', 'Espanha',
+               'Portugal', 'Holanda', 'Bélgica', 'Suíça', 'Suécia', 'Noruega', 'Dinamarca', 'Finlândia', 'Rússia', 'China', 'Japão']
+}
